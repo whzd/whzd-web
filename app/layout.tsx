54ac1b5c93465d745070849673dc1f777069190b
whzd <whzdxl@gmail.com>
2023-12-24 00:01:06 +0000

    add image; change folder structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+import '../styles/globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+      <link rel="icon" href="../public/img/favicon.ico" sizes="any" />
+
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,28 @@
-import NavBar from "./components/navbar";
-import NavItem from "./components/navitem";
+import NavBar from "../components/navbar";
+import NavItem from "../components/navitem";
 
 export default function Home() {
 return (
   <div>
   <NavBar />
     <div className="h-dvh">
-      <div className="flex justify-center text-6xl font-bold pb-5">
-        <p>Hi, I'm whzd.</p>
-      </div>
-      <div className="flex px-40 text-pretty">
-        <span className="w-2/3 text-right">🚀 Platform Engineer with 3+ years of experience and a DevOps mindset.
-        Passionate about continuous learning, I thrive on creating innovative solutions.
-        Committed to the 1% better philosophy, let's build something extraordinary together! 💻 <NavItem path="/about" displayText="Learn More"/>
-        </span>
-        <img className="w-1/3"></img>
+      <div className="flex px-80 text-pretty">
+        <div className="text-right">
+          <div className="text-6xl font-bold pb-5">
+            <p>Hi, I'm whzd.</p>
+          </div>
+          <div className="text-lg">
+            <p>Platform Engineer with 3+ years of experience and a DevOps mindset.</p>
+            <p>Passionate about continuous learning, I thrive on creating innovative solutions.</p>
+            <p>Committed to the 1% better philosophy, let's build something extraordinary together!</p>
+            <div className="flex">
+              <NavItem path="/about" displayText="Learn More"/>
+            </div>
+          </div>
+        </div>
+        <div className="w-1/3 pl-5">
+          <img className="rounded-r-full" src="/img/wiz.png"></img>
+        </div>
       </div>
     </div>
   </div>
